ca556e5da0aee5a8816c57d3200352e640ee7445
LiwiaWagner <liwia.wagner@gmail.com>
2025-01-21 16:47:20 +0100

    sidber height

--- src/components/ui/navigation/sidebar.tsx
+++ src/components/ui/navigation/sidebar.tsx
@@ -70,8 +70,8 @@ export function Sidebar() {
   return (
     <>
       <nav className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
-        <aside className="flex grow flex-col gap-y-6 overflow-y-auto border-r border-gray-200 bg-white p-4 pt-[2px] dark:border-gray-800 dark:bg-gray-950">
-          <div className="ml-1 h-16">
+        <aside className="flex grow flex-col gap-y-6 overflow-y-auto border-r border-gray-200 bg-white p-4 pt-[0px] dark:border-gray-800 dark:bg-gray-950">
+          <div className="ml-1 h-20">
             <Logo />
           </div>
           <nav
